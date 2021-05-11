61c45dc2ffdde3fe4d91c91d02e3119d4fa69674
svinohryak <shcheglovmusician@gmail.com>
2021-05-11 17:35:32 +0400

    MemoryGame basic is done

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -1,6 +1,6 @@
 let cardBox = document.querySelectorAll('.pic-box');
 let newGameBtn = document.querySelector('#new-game-btn');
-
+let numberArray = [];
 // function cardRandomiser(arr) {
 //     for(let i = 0; i < arr.length; i++) {
 //         arr[i].style.order = '';
@@ -8,8 +8,6 @@ let newGameBtn = document.querySelector('#new-game-btn');
 //     }
 // }
 
-
-let numberArray = [];
 
 function cardRandomiser(arr) {
         numberArray.splice(0);
@@ -29,90 +27,50 @@ function cardRandomiser(arr) {
 
 let cardArr = [];
 let cards = document.querySelectorAll('.card'); 
-// itworks
-// function openCard(e) {
-        // e.target.classList.add('open');
-        //проверить
-    // e.target.classList.add('open', 'block-cursor');
-//     // console.log(e.target.dataset.picName);
-//     cardArr.push(e.target.dataset.picName);
-//     e.target.removeEventListener('click', openCard);
-    
-//     console.log(cardArr);
-
-//     if(cardArr[0] == cardArr[1]) {
-//         console.log('matched');
-//     } else {
-//         console.log('not matched');
-//     }
-
-// }
-
 
 function openCard(e) {
-    // e.target.classList.add('open', 'block-cursor');
     e.target.classList.add('open');
-    // console.log(e.target.dataset.picName);
-    // cardArr.push(e.target.dataset.picName);
-    // e.target.removeEventListener('click', openCard);
-    
-    console.log(cardArr);
 
     if(cardArr.length < 2) {
         cardArr.push(e.target.dataset.picName);
         e.target.removeEventListener('click', openCard);
+
+        if(cardArr.length > 1) {
+            cards.forEach(card => card.removeEventListener('click', openCard));
+        }
+
         if((cardArr[0] == cardArr[1]) && (cardArr.length == 2)){
-            // e.target.classList.add('matched');
+            let matchedCardArr = document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`);
 
-            // let matchedPic = document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`);
-            // document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`).forEach(function(matchCards) {
-            //     matchCards.classList.add('matched');
-            // });
+            setTimeout(function() {
+                matchedCardArr.forEach(matchCards => matchCards.classList.add('matched'));
+            }, 1000);
 
-            document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`).forEach(matchCards =>
-                matchCards.classList.add('matched'));
-
-            // e.target.removeEventListener('click', openCard);
-            console.log('matched');
-            // console.log(document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`));
-            // console.log(matchedPic);
+            setTimeout(function() {
+                cards.forEach(card => card.addEventListener('click', openCard));
+            }, 1000);
+            
             cardArr.splice(0);
         } else if((cardArr[0] != cardArr[1]) && (cardArr.length == 2)){
-            console.log('not matched');
-            // e.target.removeEventListener('click', openCard);
+            let firstArrCards = document.querySelectorAll(`[data-pic-name='${cardArr[0]}']`);
+            let secondArrCards = document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`);
 
-            document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`).forEach(matchCards =>
-                matchCards.classList.remove('open'));
-            document.querySelectorAll(`[data-pic-name='${cardArr[0]}']`).forEach(matchCards =>
-                    matchCards.classList.remove('open'));
+            setTimeout(function() {
+                firstArrCards.forEach(matchCards => matchCards.classList.remove('open'));
+            }, 1000);
+            setTimeout(function() {
+                secondArrCards.forEach(matchCards => matchCards.classList.remove('open'));
+            }, 1000);
 
-            document.querySelectorAll(`[data-pic-name='${cardArr[0]}']`).forEach(anyCards =>
-                anyCards.addEventListener('click', openCard));
-            document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`).forEach(anyCards =>
-                anyCards.addEventListener('click', openCard));
 
-            // document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`).forEach(matchCards =>
-            //     matchCards.classList.add('test'));
-            // document.querySelectorAll(`[data-pic-name='${cardArr[0]}']`).forEach(matchCards =>
-            //         matchCards.classList.add('test'));
+            setTimeout(function() {
+                cards.forEach(card => card.addEventListener('click', openCard));
+            }, 1000);
 
-            // e.target.classList.remove('open');
             cardArr.splice(0);
         }
-        
-        // e.target.removeEventListener('click', openCard);
     }
-
-    // if(cardArr[0] == cardArr[1]) {
-    //     console.log('matched');
-    // } else {
-    //     console.log('not matched');
-    // }
-
 }
-
-
-
 
 
 
@@ -124,26 +82,4 @@ window.onload = function() {
     cardRandomiser(cardBox);
 }
 
-cards.forEach(function(card){
-    card.addEventListener('click', openCard)
-});
-
-// console.log(cards[1].dataset.picName);
-
-// window.addEventListener('click', (e) => {
-//     console.log(e.target);
-// });
-// window.addEventListener('click', (e) => {
-//     console.log(e.target.classList.toggle('open'));
-// })
-
-// let test = document.querySelectorAll("[data-pic-name='kura']");
-// let test2 = document.querySelectorAll("[data-pic-name='mouse']");
-// let test3 = document.querySelectorAll("[data-pic-name='svin']");
-// console.log(test);
-// console.log(test2);
-// console.log(test3);
-// window.addEventListener('click', (e) => {
-//     console.log(document.querySelector('[data-pic-name]'));
-// });
-// document.querySelector('[someAttr]')
+cards.forEach(card => card.addEventListener('click', openCard));
