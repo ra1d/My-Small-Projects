d530edb243a05dfc20a059bcb76385f952345834
svinohryak <shcheglovmusician@gmail.com>
2021-05-13 17:00:39 +0400

    MemoryGame is ready

--- MemoryGame/js/script.js
+++ MemoryGame/js/script.js
@@ -5,8 +5,13 @@ let cards = document.querySelectorAll('.card');
 let input = document.querySelector('#input');
 let cardArr = [];
 let numberArray = [];
+let matchedCardsNumber = 0;
 
-
+const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.modal__close-btn');
+const modalNewGameBtn = document.querySelector('.modal__new-game-btn');
+const overlay = document.querySelector('#overlay');
+const modalMovesAmount = document.querySelector('.moves-count');
 
 // простой рандомайзер
 // function cardRandomiser(arr) {
@@ -58,6 +63,13 @@ function openCard(e) {
 
             moveCount(input);
             cardArr.splice(0);
+
+            addMatchedCardsNumber();
+            if(matchedCardsNumber === (cardBox.length / 2)) {
+                console.log('you win!');
+                setTimeout(() => openModal(), 1500);
+                modalMovesAmount.textContent = `сделано ходов: ${input.value}`;
+            }
         } else if((cardArr[0] != cardArr[1]) && (cardArr.length == 2)){
             let firstArrCards = document.querySelectorAll(`[data-pic-name='${cardArr[0]}']`);
             let secondArrCards = document.querySelectorAll(`[data-pic-name='${cardArr[1]}']`);
@@ -89,6 +101,7 @@ function newGameFn() {
     setTimeout(() => cardRandomiser(cardBox), 500);
     cards.forEach(card => card.addEventListener('click', openCard));
     input.value = '0';
+    matchedCardsNumber = 0;
 }
 
 function giveUpFn() {
@@ -101,6 +114,32 @@ function moveCount(inpValue) {
     x = Number(x) + 1;
     inpValue.value = x;
 }
+
+
+function openModal() {
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal() {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+function newGameModal() {
+    closeModal();
+    newGameFn();
+}
+
+function addMatchedCardsNumber() {
+    matchedCardsNumber += 1;
+    console.log(matchedCardsNumber);
+}
+
+overlay.addEventListener('click', closeModal);
+
+modalCloseBtn.addEventListener('click', closeModal);
+modalNewGameBtn.addEventListener('click', newGameModal);
 
 newGameBtn.addEventListener('click', newGameFn);
 
